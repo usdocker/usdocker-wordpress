257e9f6b557676a4390c52f5aadd03e2b76834ea
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-08-19 12:58:09 -0300

    Adapt scripts for the new Container template

--- usdocker_wordpress.js
+++ usdocker_wordpress.js
@@ -8,12 +8,13 @@ const SCRIPTNAME = 'wordpress';
 
 let config = usdocker.config(SCRIPTNAME);
 let configGlobal = usdocker.configGlobal();
+const CONTAINERNAME = SCRIPTNAME + configGlobal.get('container-suffix');
 
 function getContainerDef() {
 
     let docker = usdocker.dockerRunWrapper(configGlobal);
     return docker
-        .containerName(SCRIPTNAME + configGlobal.get('container-suffix'))
+        .containerName(CONTAINERNAME)
         .port(config.get('port'), 80)
         .volume(config.get('folder'), '/data')
         .volume(config.get('pluginFolder'), '/var/www/html/wp-content/plugins')
@@ -74,20 +75,20 @@ module.exports = {
 
     up: function(callback)
     {
-        usdocker.up(SCRIPTNAME, getContainerDef(), callback);
+        usdocker.up(CONTAINERNAME, getContainerDef(), callback);
     },
 
     status: function(callback) {
-        usdocker.status(SCRIPTNAME, callback);
+        usdocker.status(CONTAINERNAME, callback);
     },
 
     down: function(callback)
     {
-        usdocker.down(SCRIPTNAME, callback);
+        usdocker.down(CONTAINERNAME, callback);
     },
 
     restart: function(callback)
     {
-        usdocker.restart(SCRIPTNAME, getContainerDef(), callback);
+        usdocker.restart(CONTAINERNAME, getContainerDef(), callback);
     }
 };
